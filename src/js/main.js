2ce75b7bdf516b965bbf0e250f657f21fba5e6d2
Kate Shirley <kshirley@88brandpartners.com>
2016-02-17 17:35:48 -0600

    fixed column and nested bootstrap

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,10 @@ $(function () { // wait for document ready
 		// build scene
 
 
-		var scene = new ScrollMagic.Scene({triggerElement: "#pin2"})
-						.setPin("#pin2")
-						.addIndicators({name: "2 (duration: 0)"}) // add indicators (requires plugin)
-						.addTo(controller);
+		// var scene = new ScrollMagic.Scene({triggerElement: "#pin2"})
+		// 				.setPin("#pin2")
+		// 				.addIndicators({name: "2 (duration: 0)"}) // add indicators (requires plugin)
+		// 				.addTo(controller);
 
 
 		new ScrollMagic.Scene({triggerElement: "#sec1", duration: 500})
