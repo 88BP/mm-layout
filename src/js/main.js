a45833ccad5853b0d5f8bf214c7c6780167f777c
Kate Shirley <kshirley@88brandpartners.com>
2016-02-18 17:26:11 -0600

    scrolling colors

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,34 @@
 
 $(function () { // wait for document ready
 
+
+
+		//backgroundTweens
+
+	
+
 		// init controller
 		var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: 0.3}});
+
+			var bgTween1 = TweenMax.to('.body', 1, {
+			backgroundColor: '#ea204c', 
+			ease: Linear.easeNone
+			});
+
+			var bgTween2 = TweenMax.to('.body', 1, {
+			backgroundColor: '#f9e11b', 
+			ease: Linear.easeNone
+			});
+
+			var bgTween3 = TweenMax.to('.body', 1, {
+			backgroundColor: '#f7db69', 
+			ease: Linear.easeNone
+			});
+
+			var bgTween4 = TweenMax.to('.body', 1, {
+			backgroundColor: '#2d9598', 
+			ease: Linear.easeNone
+			});
 		
 		// build scene
 
@@ -13,21 +39,25 @@ $(function () { // wait for document ready
 
 		new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 700})
 					.setPin("#pin1", {spacerClass:"spacerClass1"})
+					.setTween(bgTween1)
 					.addIndicators({name: "1 (duration: 700)"})
 					.addTo(controller);
 
 		new ScrollMagic.Scene({triggerElement: "#trigger2", duration: 700})
 					.setPin("#pin2")
+					.setTween(bgTween2)
 					.addIndicators({name: "2 (duration: 700)"})
 					.addTo(controller);
 
 		new ScrollMagic.Scene({triggerElement: "#trigger3", duration: 700})
 					.setPin("#pin3")
+					.setTween(bgTween3)
 					.addIndicators({name: "3 (duration: 700)"})
 					.addTo(controller);
 
 		new ScrollMagic.Scene({triggerElement: "#trigger4", duration: 700})
 					.setPin("#pin4")
+					.setTween(bgTween4)
 					.addIndicators({name: "4 (duration: 700)"})
 					.addTo(controller);
 
@@ -42,7 +72,7 @@ $(function () { // wait for document ready
 					.addIndicators() // add indicators (requires plugin)
 					.addTo(controller);
 		new ScrollMagic.Scene({triggerElement: "#sec3", duration: 1000})
-					.setClassToggle("#high3", "active") // add class toggle
+					.setClassToggle("#high3", "active") // add class toggl
 					.addIndicators() // add indicators (requires plugin)
 					.addTo(controller);
 		new ScrollMagic.Scene({triggerElement: "#sec4", duration: 1000})
