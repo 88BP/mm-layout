565a5414302ff9eb674d9734ad3b59a5f9c12306
Kate Shirley <kshirley@88brandpartners.com>
2016-02-18 16:01:02 -0600

    all supporting info pinned, global triggerhook, anchorlinks

--- src/js/main.js
+++ src/js/main.js
@@ -2,35 +2,50 @@
 $(function () { // wait for document ready
 
 		// init controller
-		var controller = new ScrollMagic.Controller();
+		var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: 0.3}});
 		
 		// build scene
 
-		new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 800})
-					.setPin("#pin1")
-					.addIndicators({name: "1 (duration: 800)"})
+		new ScrollMagic.Scene({triggerElement: "#menuTrigger"})
+					.setPin("#menuPin")
+					.addIndicators()
 					.addTo(controller);
 
-		new ScrollMagic.Scene({triggerElement: "#trigger2", duration: 600})
+		new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 700})
+					.setPin("#pin1", {spacerClass:"spacerClass1"})
+					.addIndicators({name: "1 (duration: 700)"})
+					.addTo(controller);
+
+		new ScrollMagic.Scene({triggerElement: "#trigger2", duration: 700})
 					.setPin("#pin2")
-					.addIndicators({name: "1 (duration: 600)"})
+					.addIndicators({name: "2 (duration: 700)"})
+					.addTo(controller);
+
+		new ScrollMagic.Scene({triggerElement: "#trigger3", duration: 700})
+					.setPin("#pin3")
+					.addIndicators({name: "3 (duration: 700)"})
+					.addTo(controller);
+
+		new ScrollMagic.Scene({triggerElement: "#trigger4", duration: 700})
+					.setPin("#pin4")
+					.addIndicators({name: "4 (duration: 700)"})
 					.addTo(controller);
 
 
 
-		new ScrollMagic.Scene({triggerElement: "#sec1", duration: 500})
+		new ScrollMagic.Scene({triggerElement: "#sec1", duration: 1000})
 					.setClassToggle("#high1", "active") // add class toggle
 					.addIndicators() // add indicators (requires plugin)
 					.addTo(controller);
-		new ScrollMagic.Scene({triggerElement: "#sec2", duration: 500})
+		new ScrollMagic.Scene({triggerElement: "#sec2", duration: 1000})
 					.setClassToggle("#high2", "active") // add class toggle
 					.addIndicators() // add indicators (requires plugin)
 					.addTo(controller);
-		new ScrollMagic.Scene({triggerElement: "#sec3", duration: 500})
+		new ScrollMagic.Scene({triggerElement: "#sec3", duration: 1000})
 					.setClassToggle("#high3", "active") // add class toggle
 					.addIndicators() // add indicators (requires plugin)
 					.addTo(controller);
-		new ScrollMagic.Scene({triggerElement: "#sec4", duration: 500})
+		new ScrollMagic.Scene({triggerElement: "#sec4", duration: 1000})
 					.setClassToggle("#high4", "active") // add class toggle
 					.addIndicators() // add indicators (requires plugin)
 					.addTo(controller);
