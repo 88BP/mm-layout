bd8f9b5b4b64cdcead931033a7454208fb378d24
Kate Shirley <kshirley@88brandpartners.com>
2016-02-19 17:42:11 -0600

    image slide fade in

--- src/js/main.js
+++ src/js/main.js
@@ -10,27 +10,41 @@ $(function () { // wait for document ready
 		// init controller
 		var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: 0.3}});
 
+		var bgImg = ('.bgImg');
+		TweenMax.set(bgImg,{autoAlpha:0});
+	
+
+
 			var bgTween1 = TweenMax.to('.body', 1, {
+			backgroundColor: '#a8216b', 
+			ease: Linear.easeNone
+			});
+
+			var bgTween2 = TweenMax.to ('.body', 1, {
+			backgroundColor: '#2d9598', 
+			ease: Linear.easeNone
+			});
+			
+		
+
+			var bgTween3 = TweenMax.to('.body', 1, {
 			backgroundColor: '#ea204c', 
 			ease: Linear.easeNone
 			});
 
-			var bgTween2 = TweenMax.to('.body', 1, {
-			backgroundColor: '#f9e11b', 
-			ease: Linear.easeNone
+			var bgTween4 = TweenMax.to ('.bgImg', 1, {
+			autoAlpha:1 
 			});
 
-			var bgTween3 = TweenMax.to('.body', 1, {
-			backgroundColor: '#f7db69', 
-			ease: Linear.easeNone
-			});
-
-			var bgTween4 = TweenMax.to('.body', 1, {
-			backgroundColor: '#2d9598', 
-			ease: Linear.easeNone
-			});
+			
 		
 		// build scene
+
+		new ScrollMagic.Scene({triggerElement: "#titleTrigger", duration: 700})
+					.offset(300)
+					.setPin(".titleImg", {spacerClass:"titleSpacer"})
+					.addIndicators({name: "title trigger"})
+					.addTo(controller);
 
 		new ScrollMagic.Scene({triggerElement: "#menuTrigger"})
 					.setPin("#menuPin")
@@ -39,26 +53,43 @@ $(function () { // wait for document ready
 
 		new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 700})
 					.setPin("#pin1", {spacerClass:"spacerClass1"})
-					.setTween(bgTween1)
 					.addIndicators({name: "1 (duration: 700)"})
 					.addTo(controller);
 
 		new ScrollMagic.Scene({triggerElement: "#trigger2", duration: 700})
 					.setPin("#pin2")
-					.setTween(bgTween2)
 					.addIndicators({name: "2 (duration: 700)"})
 					.addTo(controller);
 
 		new ScrollMagic.Scene({triggerElement: "#trigger3", duration: 700})
 					.setPin("#pin3")
-					.setTween(bgTween3)
 					.addIndicators({name: "3 (duration: 700)"})
 					.addTo(controller);
 
 		new ScrollMagic.Scene({triggerElement: "#trigger4", duration: 700})
 					.setPin("#pin4")
-					.setTween(bgTween4)
 					.addIndicators({name: "4 (duration: 700)"})
+					.addTo(controller);
+
+
+		new ScrollMagic.Scene({triggerElement: "#bgTrigger1", duration: 300})
+					.setTween(bgTween1)
+					.addIndicators({name: "bgTrigger1 (duration: 100)"})
+					.addTo(controller);
+
+		new ScrollMagic.Scene({triggerElement: "#bgTrigger2", duration: 300})
+					.setTween(bgTween2)
+					.addIndicators({name: "bgTrigger2 (duration: 100)"})
+					.addTo(controller);
+
+		new ScrollMagic.Scene({triggerElement: "#bgTrigger3", duration: 300})
+					.setTween(bgTween3)
+					.addIndicators({name: "bgTrigger3 (duration: 100)"})
+					.addTo(controller);
+
+		new ScrollMagic.Scene({triggerElement: "#bgTrigger4", duration: 300})
+					.setTween(bgTween4)
+					.addIndicators({name: "bgTrigger4(duration: 100)"})
 					.addTo(controller);
 
 
@@ -80,6 +111,21 @@ $(function () { // wait for document ready
 					.addIndicators() // add indicators (requires plugin)
 					.addTo(controller);
 	});
+
+
+
+
+
+
+
+				// var bgTween2 = TweenMax.to($bgImg, 1, {
+			// opacity: 1, 
+			// ease: Linear.easeNone
+			// });
+
+
+
+
 
 // 	//init animation for .5 seconds 
 // 	var tween = TweenMax.to('#animation', 0.5, {
